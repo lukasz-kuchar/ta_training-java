aba98c8e24cc2dceaca2155ac4780e8606ad724e
Lukasz Kuchar <lukaszkuchar1@gmail.com>
2023-07-26 15:47:12 +0200

    HelloAutocode java solution added

---
+++ Homework/src/com/epam/training/lukasz_kuchar/HelloAutocode.java
@@ -0,0 +1,9 @@
+package com.epam.training.lukasz_kuchar;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        //Write a program, printing "Hello, Autocode!"
+        System.out.println("Hello, Autocode!");
+    }
+}
+
